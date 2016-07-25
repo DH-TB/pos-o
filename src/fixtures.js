a61f0873653e7792f756037e9875897de99964c5
DH-TB <929659475@qq.com>
2016-07-25 20:38:59 +0800

    replace promotion object by Promotin instance

--- src/fixtures.js
+++ src/fixtures.js
@@ -1,4 +1,5 @@
 const Item = require('./models/Item');
+const Promotion = require('./models/Promotion');
 
 function loadAllItems() {
   return [
@@ -13,14 +14,7 @@ function loadAllItems() {
 
 function loadPromotions() {
   return [
-    {
-      type: 'BUY_TWO_GET_ONE_FREE',
-      barcodes: [
-        'ITEM000000',
-        'ITEM000001',
-        'ITEM000005'
-      ]
-    }
+    new Promotion('BUY_TWO_GET_ONE_FREE',['ITEM000000', 'ITEM000001', 'ITEM000005']) 
   ];
 }
 
